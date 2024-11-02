cce3fea4635f0d7b7023d3338297ddee37a2d2eb
mrleos <rmuhreksa@gmail.com>
2024-11-02 23:31:05 +0800

    chatbot done

--- routes/web.php
+++ routes/web.php
@@ -4,14 +4,35 @@ use App\Http\Controllers\ChatbotController;
 use App\Http\Controllers\LocationController;
 use Illuminate\Support\Facades\Route;
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
+Route::get('/', function () {
+    return view('welcome');
+})->name('home.index');
 
-Route::get('/chatbot', [ChatbotController::class, 'index']);
+Route::get('/chatbot', [ChatbotController::class, 'index'])->name('chatbot.index');
 // Route::post('/chatbot/send', [ChatbotController::class, 'sendMessage']);
 Route::get('/search-facilities', [ChatbotController::class, 'searchHealthcareFacilities']);
 
-Route::get('/', function () {
+Route::get('/kamus', function () {
     return view('kamus');
 });
+
+// use Illuminate\Http\Request;
+// use Illuminate\Support\Facades\Http;
+
+// Route::get('/proxy/nearbysearch', function (Request $request) {
+//     $latitude = $request->input('latitude');
+//     $longitude = $request->input('longitude');
+//     $type = $request->input('type');
+//     $radius = 5000;
+//     $apiKey = env('GOOGLE_MAPS_API_KEY');
+
+//     $response = Http::get("https://maps.googleapis.com/maps/api/place/nearbysearch/json", [
+//         'location' => "$latitude,$longitude",
+//         'radius' => $radius,
+//         'type' => $type,
+//         'key' => $apiKey,
+//     ]);
+
+//     return $response->json();
+// });
+
